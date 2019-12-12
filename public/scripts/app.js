e194d800174ac5514b231aabc142e2224b9ed977
Sumit Mukhija <sumitmukhija@hotmail.com>
2019-12-12 15:00:51 +0000

    Basic functionality to add done. Need to refactor CSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,59 @@
-'use strict';
+"use strict";
 
-var rootElement = document.querySelector('#root');
+var rootElement = document.querySelector('.container');
+
+var list = [];
+
+var getList = function getList() {
+    return list.map(function (item) {
+        return React.createElement(
+            "li",
+            null,
+            item
+        );
+    });
+};
+
+var onFormSubmission = function onFormSubmission(e) {
+    e.preventDefault();
+    var item = e.target.elements.intext.value;
+    if (item) {
+        list.push(item);
+        e.target.elements.intext.value = "";
+    }
+    render();
+};
+
+var render = function render() {
+    var listComponent = React.createElement(
+        "div",
+        { className: "notepad card" },
+        React.createElement(
+            "div",
+            { className: "list-area" },
+            React.createElement(
+                "ul",
+                null,
+                getList()
+            )
+        ),
+        React.createElement(
+            "div",
+            { className: "control-area" },
+            React.createElement(
+                "form",
+                { onSubmit: onFormSubmission },
+                React.createElement("input", { type: "text", name: "intext" }),
+                React.createElement(
+                    "button",
+                    null,
+                    "\u2191"
+                )
+            )
+        )
+    );
+
+    ReactDOM.render(listComponent, rootElement);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
-const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector('.container');
+
+
+
+let list = [];
+
+
+const getList = () => {
+    return list.map((item) => { 
+        return <li>{item}</li>
+    });
+}
+
+const onFormSubmission = (e) => {
+    e.preventDefault();
+    const item = e.target.elements.intext.value;
+    if (item) {
+        list.push(item);
+        e.target.elements.intext.value = ""
+    }
+    render();
+}
+
+const render = () => {
+    const listComponent = (
+
+        <div className="notepad card">
+            <div className="list-area">
+                <ul>
+                    {getList()}
+                </ul>
+            </div>
+            <div className="control-area">
+                <form onSubmit={onFormSubmission}>
+                    <input type="text" name="intext"></input>
+                        <button>↑</button>
+                    </form>
+                </div>
+            </div>
+    );
+    
+    ReactDOM.render(listComponent, rootElement);    
+}
+
+render();
+
