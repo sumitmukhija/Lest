eeb0a80edbb1ea0892e4a186aab65701106d747c
Sumit Mukhija <sumitmukhija@hotmail.com>
2019-12-16 21:09:00 +0000

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/app.js",
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: 'bundle.js'
+    }
+}
